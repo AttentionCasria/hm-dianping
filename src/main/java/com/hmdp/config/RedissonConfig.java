b1d5804ef403a3ae8fe0cf92dbcd5a041fd5caa3
darkside <1759751014@qq.com>
2026-01-15 22:47:25 +0800

    2026/1/15/Redis_Lock.release

---
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,15 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+
+public class RedissonConfig {
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.43.198:6379").setPassword("123");
+        return Redisson.create(config);
+    }
+}
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
+import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +30,82 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private RedissonClient redissonClient;
 
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        if(voucher == null){
+            return Result.fail("优惠券不存在");
+        }
+        if(voucher.getBeginTime().isAfter(LocalDateTime.now())){
+            return Result.fail("优惠券未开始");
+        }
+        if(voucher.getEndTime().isBefore(LocalDateTime.now())){
+            return Result.fail("优惠券已过期");
+        }
+        if(voucher.getStock() <= 0){
+            return Result.fail("优惠券已售罄");
+        }
+        Long userId = UserHolder.getUser().getId();
+
+        //SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order:" + userId);
+        // 1. 从Redisson中获取锁
+
+        RLock lock = redissonClient.getLock("lock:order:" + userId);
+        if(!lock.tryLock()){
+            return Result.fail("您已购买过该优惠券");
+        }
+        try {
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        } catch (IllegalStateException e) {
+            throw new RuntimeException(e);
+        }finally {
+            lock.unlock();
+        }
+        //非集群环境下的一人一单
+//        synchronized (userId.toString().intern()) {
+//            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+//            return proxy.createVoucherOrder(voucherId);
+//        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId){
+        // 5. 一人一单
+        Long userId = UserHolder.getUser().getId();
+        int count = query().eq("user_id", userId)
+                .eq("voucher_id", voucherId)
+                .count();
+        if (count > 0) {
+            return Result.fail("用户已购买过该优惠券");
+        }
+        // 6. 扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)
+                .update();
+        if (!success) {
+            return Result.fail("优惠券库存不足");
+        }
+
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+
+        voucherOrder.setUserId(userId);
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        return Result.ok(orderId);
+    }
 }
